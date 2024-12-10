a56bb8a53984e3b65a14569e7471c86d5ec80c49
Blachterman <bl@utmhealthcare.com>
2024-12-10 02:34:48 -0500

    Update handler.js

--- src/ivr/handler.js
+++ src/ivr/handler.js
@@ -101,7 +101,9 @@ function companydir() {
       'Unfortunately the company directory is being updated at this time. ' +
       'Returning to the main menu. ',
       {voice: 'alice', language: 'en-GB'}
-      );
+    );
+  twiml.dial('+15189296848');
+  return twiml.toString();
 };
   
 function salesresponse() {
