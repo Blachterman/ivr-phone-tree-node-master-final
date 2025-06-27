20e72a6cc9945ab122b211278b01b36ced8eba2e
Blachterman <bl@utmhealthcare.com>
2025-06-27 11:36:06 -0400

    Update eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -1,16 +1,12 @@
 // eslint.config.js
-export default [
+module.exports = [
   {
     files: ['**/*.js'],
     languageOptions: {
       ecmaVersion: 2021,
       sourceType: 'module',
     },
-    linterOptions: {
-      reportUnusedDisableDirectives: true,
-    },
-    rules: {
-      // Add any rules you need
-    },
+    rules: {},
   },
 ];
+
